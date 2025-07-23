a954423924899d26d2bce0679b3692111e0e1e1e
Vachi Kalra <vachikalra@gmail.com>
2025-07-23 13:38:34 -0700

    Initial Commit

---
+++ InteractiveUI/InteractiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIApp.swift
+//  InteractiveUI
+//
+//  Created by Vachi Kalra on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InteractiveUITests/InteractiveUITests.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUITests.swift
+//  InteractiveUITests
+//
+//  Created by Vachi Kalra on 7/23/25.
+//
+
+import Testing
+@testable import InteractiveUI
+
+struct InteractiveUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
